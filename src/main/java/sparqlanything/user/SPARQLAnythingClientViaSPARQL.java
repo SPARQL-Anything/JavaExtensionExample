ca3101175fe4f92217e7f7077ca60c66a8e6d7c7
Luigi Asprino <luigi.asprino@gmail.com>
2024-12-04 15:02:14 +0100

    Add function and magic property

--- src/main/java/sparqlanything/user/SPARQLAnythingClientViaSPARQL.java
+++ src/main/java/sparqlanything/user/SPARQLAnythingClientViaSPARQL.java
@@ -1,13 +1,20 @@
 package sparqlanything.user ;
 
 import io.github.sparqlanything.engine.FacadeX;
-import io.github.sparqlanything.engine.TriplifierRegisterException;
+import io.github.sparqlanything.model.TriplifierRegisterException;
 import org.apache.jena.query.*;
 import org.apache.jena.sparql.engine.main.QC;
+import org.apache.jena.sparql.function.FunctionRegistry;
+import org.apache.jena.sparql.pfunction.PropertyFunctionRegistry;
+import org.apache.jena.sys.JenaSystem;
 
 public class SPARQLAnythingClientViaSPARQL {
 
-	public static void main(String[] args) throws TriplifierRegisterException {
+	public static void main(String[] args) throws TriplifierRegisterException, TriplifierRegisterException {
+
+		System.out.println(PropertyFunctionRegistry.chooseRegistry(ARQ.getContext()));
+
+		JenaSystem.init();
 
 		// Set FacadeX OpExecutor as default executor factory
 		QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
@@ -15,18 +22,26 @@ public class SPARQLAnythingClientViaSPARQL {
 		// Register the new Triplifier
 		FacadeX.Registry.registerTriplifier(MyTriplifier.class.getCanonicalName(), new String[]{"myext"}, new String[]{"my-mime-type"});
 
+		// Register the new function
+		FunctionRegistry.get().put("http://example.org/theAnswer", TheAnswer.class);
+
+		// Register the new magic property
+		PropertyFunctionRegistry.get().put("http://example.org/assign42", Assign42.class);
+
 		// Execute the query by using standard Jena ARQ's API
 		Dataset kb = DatasetFactory.createGeneral();
 
 		Query query = QueryFactory.create(
 						"PREFIX fx:  <http://sparql.xyz/facade-x/ns/> " +
 						"PREFIX xyz: <http://sparql.xyz/facade-x/data/> " +
-						"SELECT ?slotNumber ?o { " +
+						"SELECT ?slotNumber ?o ?assignment ?answer{ " +
 								"SERVICE <x-sparql-anything:> { " +
 									"fx:properties fx:content 'abc' ; " +
 										"fx:media-type 'my-mime-type' . " +
-									"?s ?p ?o " +
+									"?s ?p ?o ." +
+									"?s <http://example.org/assign42> ?assignment " +
 									"BIND(fx:cardinal(?p) AS ?slotNumber) " +
+									"BIND(<http://example.org/theAnswer>(?p) AS ?answer) " +
 									"FILTER(BOUND(?slotNumber))" +
 								"}}");
 
